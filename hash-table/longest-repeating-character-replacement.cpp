9b1888ec6b2080190b96620506188246276eae84
Gaurav Mer <mergaurav4@gmail.com>
2026-06-01 17:38:15 +0530

    LC-longest-repeating-character-replacement [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-repeating-character-replacement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int maxele = 0;
+        int maxlen = 0;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            mpp[s[right]]++;
+            maxele = max(maxele, mpp[s[right]]);
+            while ((right - left + 1) - maxele > k) {
+                mpp[s[left]]--;
+                left++;
+            }
+            maxlen = max(maxlen, right - left + 1);
+        }
+        return maxlen
+    }
+};
