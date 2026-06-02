bcabe38675acaf81e6e7048faf61857d1865a431
Gaurav Mer <mergaurav4@gmail.com>
2026-06-02 10:31:17 +0530

    LC-binary-tree-right-side-view [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/binary-tree-right-side-view.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        map<int, int> mpp;
+        if(!root) return {};
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            TreeNode* node = it.first;
+            int level = it.second;
+            if (mpp.find(level) == mpp.end()) {
+                mpp[level] = node->val;
+            } else if (mpp.find(level) != mpp.end()) {
+                mpp[level] = node->val;
+            }
+            if (node->left)
+                q.push({node->left, level + 1});
+            if (node->right)
+                q.push({node->right, level + 1});
+        }
+        vector<int> ans;
+        for (auto it : mpp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
