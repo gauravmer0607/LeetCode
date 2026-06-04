feda2d491884d33cfcccaddd579bd003c7b17ce5
Gaurav Mer <mergaurav4@gmail.com>
2026-06-04 20:27:03 +0530

    LC-maximum-width-of-binary-tree [Runtime: N/A; Memory: N/A]

--- tree/maximum-width-of-binary-tree.cpp
+++ tree/maximum-width-of-binary-tree.cpp
@@ -20,9 +20,9 @@ public:
         q.push({root, 0});
         while (!q.empty()) {
             int size = q.size();
-            long long mini = q.front().second();
+            long long mini = q.front().second;
             long long first, last;
-            for (int i = 0; i < size(); i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front().first;
                 long long curr = q.front().second - mini;
                 q.pop();
