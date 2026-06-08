3649cdd370767b3c5f74036df0248962d1bb802d
Gaurav Mer <mergaurav4@gmail.com>
2026-06-08 23:47:50 +0530

    LC-insert-into-a-binary-search-tree [Runtime: N/A; Memory: N/A]

--- tree/insert-into-a-binary-search-tree.cpp
+++ tree/insert-into-a-binary-search-tree.cpp
@@ -6,30 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root==NULL){
+        if (root == NULL) {
             return new TreeNode(val);
         }
-        TreeNode* curr=root;
-        while(true){
-            if(val<curr->val){
-                if(curr->left==NULL){
-                    curr->left=new TreeNode(val);
+        TreeNode* curr = root;
+        while (true) {
+            if (val < curr->val) {
+                if (curr->left == NULL) {
+                    curr->left = new TreeNode(val);
                     break;
                 }
-                curr=curr->left;
-            }
-            else{
-                if(curr->right==NULL){
-                    curr->right=new TreeNode(val);
+                curr = curr->left;
+            } else {
+                if (curr->right == NULL) {
+                    curr->right = new TreeNode(val);
                     break;
                 }
-                curr=curr->right;
+                curr = curr->right;
             }
         }
         return root;
