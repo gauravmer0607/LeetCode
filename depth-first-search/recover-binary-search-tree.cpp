76e4615a1f08dcff4a82f9ba8827ea62a5d73303
Gaurav Mer <mergaurav4@gmail.com>
2026-06-10 19:07:15 +0530

    LC-recover-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/recover-binary-search-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = NULL;
+    TreeNode* first = NULL;
+    TreeNode* last = NULL;
+
+    void inorder(TreeNode* root) {
+        if (!root)
+            return;
+        inorder(root->left);
+        if (prev && prev->val > root->val) {
+            if (!first)
+                first = prev;
+            last = root;
+        }
+        prev = root;
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val, last->val);
+    }
+};
